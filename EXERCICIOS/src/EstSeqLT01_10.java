0dc4a69c7dd6bc66f01fa1c832d167c1c88dfa9d
vianathecreator <felipeviana2501@gmail.com>
2026-09-12 17:51:00 -0300

    Exercicio 10 resolvido

--- EXERCICIOS/src/EstSeqLT01_10.java
+++ EXERCICIOS/src/EstSeqLT01_10.java
@@ -1,10 +1,10 @@
-import javax.swing.JOptionPane
+import javax.swing.JOptionPane;
 public class EstSeqLT01_10 {
     public void main (String[] args) {
         double a, b, diff;
         a = Double.parseDouble (JOptionPane.showInputDialog(null, "Digite o primeiro número: "));
         b = Double.parseDouble (JOptionPane.showInputDialog(null, "Digite o segundo número: "));
         diff = a - b;
-        JOptionPane.showMessageDialog(null, "A diferença entre eles é de: " + diff);
+        JOptionPane.showMessageDialog(null,String.format("A diferença entre eles é de: %.0f", diff));
     }
 }
