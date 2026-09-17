c799a238d1747282d6450c892f9818d753c30941
Felipe Viana <felipeviana2501@gmail.com>
2026-09-17 14:25:11 -0300

    resolvido exercicio 21

---
+++ EXERCICIOS/src/EstSeqLT01_21.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+public class EstSeqLT01_21 {
+    static double nota1, nota2, nota3, nota4, media;
+
+    public static void ReceberNotas() {
+        nota1 = Double.parseDouble (JOptionPane.showInputDialog("Digite a nota do primeiro bimestre: "));
+        nota2 = Double.parseDouble (JOptionPane.showInputDialog("Digite a nota do segundo bimestre: "));
+        nota3 = Double.parseDouble (JOptionPane.showInputDialog("Digite a nota do terceiro bimestre: "));
+        nota4 = Double.parseDouble (JOptionPane.showInputDialog("Digite a nota do quarto bimestre: "));
+    }
+    
+    public static void CalculoMedia() {
+        media = (nota1 + nota2 + nota3 + nota4) / 2;
+    }
+
+    public static void main (String[] args) {
+        ReceberNotas();
+        CalculoMedia();
+        if (media >= 6.0) {
+            JOptionPane.showMessageDialog(null, "APROVADO!");
+        } else if (media >= 3.0 || media < 6.0) {
+            JOptionPane.showMessageDialog(null, "EXAME!");
+        } else if (media < 3.0) {
+            JOptionPane.showMessageDialog(null, "RETIDO!");
+        }
+    }
+}
